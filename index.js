5d4a15c3c8ae943a0afb768b6a602939cb577174
Elton <eltonnuness@users.noreply.github.com>
2019-06-26 00:19:42 -0300

    Days of each month

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const y = require('./lib/year.js');
+
+const inputYear = 2019;
+const inputMonth = "June";
+const inputDay = 25;
+
+const todayYear = y.instanceYear(inputYear);
+
+console.log(JSON.stringify(todayYear));
+
+
+
+
+
